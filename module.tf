c07a5f7c9306d09df626c1d179359ac3799ebb4b
Brett OdgersJewell <Brett.OdgersJewell@insearch.edu.au>
2023-11-03 05:57:07 +0800

    update module

--- module.tf
+++ module.tf
@@ -44,7 +44,7 @@ dynamic "identity"  {
     for_each = var.identity != null ? [var.identity] : []
     content {
       type = var.identity.type
-      identity_ids = [var.identity.ids]
+      identity_ids = var.identity.ids
   }
 }
 
